755cbe943b85326b4d2389b6c8e1dd38824d8dd0
Deepraj Das <mail30deeprajdas@gmail.com>
2024-05-26 21:40:48 +0530

    first commit

--- main.js
+++ main.js
@@ -4,31 +4,3 @@ document.addEventListener("DOMContentLoaded", function () {
     video.playbackRate = 0.5; // Set playback speed to 0.5x
   });
 });
-
-function smoothScrollToSection(sectionId) {
-  var target = $("#" + sectionId);
-  if (target.length) {
-    $("html, body").animate(
-      {
-        scrollTop: target.offset().top,
-      },
-      2000
-    );
-    return;
-  }
-}
-
-const arrowOnClickHandler = () => {
-  console.log(window.scrollY);
-  if (window.scrollY == 0) {
-    smoothScrollToSection("home");
-  } else if (window.scrollY >= 80 && window.scrollY < 1154) {
-    smoothScrollToSection("about");
-  } else if (window.scrollY >= 1154 && window.scrollY < 2130) {
-    smoothScrollToSection("projects");
-  } else if (window.scrollY >= 2130 && window.scrollY < 3106) {
-    smoothScrollToSection("skills");
-  } else if (window.scrollY >= 3106 && window.scrollY < 4082) {
-    smoothScrollToSection("contact");
-  }
-};
